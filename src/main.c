63228e6ddbfafa27d994990791d68ce4642584aa
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-05-21 15:44:23 +0000

    added printf

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include "../include/hash.h"
 
 int main() {
     ht_hash_table *ht = ht_new();
-    ht_del_hash_table(ht);
+    ht_insert(ht, "name", "Hermoine");
+    printf("Name: %s\n", ht_search(ht, "name"));
+    return 0;
 }
